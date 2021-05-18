c748714d3b9910f14d9bb315ac26a2439be29d3f
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-05-18 06:06:02 +0000

    update

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,12 +1,33 @@
 """ 
 Implmentation of a HashTable in python.
 @author HELEGAH RAYNARD DODZI
+@email dodzireynard@gmail.com
 @date 06.05.2021
 """
 
 class HashTable:
-    def __init__(self):
-        self.table = [None for _ in range(65599)]
+    """ Creating a new HashTable
+            hash_table = HashTable()
+
+        New HashTable with a specified initial size.
+            hash_table = HashTable(65599)
+
+        Inserting an item
+            hash_table.insert(key, value)
+
+        Retrieving an item
+            hash_table.get(key)
+
+        Deleting an item
+            hash_table.delete(key)
+
+        Getting the size of the hashtable
+            len(hash_table)
+
+    """
+
+    def __init__(self, size=65599):
+        self.table = [None for _ in range(size)]
         self.table_size = 0
 
     def _get_hash(self, key):
@@ -28,6 +49,8 @@ class HashTable:
                     self.insert(key, value) 
 
     def insert(self, key, value):
+        '''Insert a new item with key and value into the hashtable.
+        '''
         index = self._get_hash(key)
         if self.table[index]:
             for item in self.table[index]:
@@ -45,6 +68,8 @@ class HashTable:
             self._resize(2)
 
     def delete(self, key):
+        '''Remove an item with speficied key from the hash table.
+        '''
         index = self._get_hash(key)
         if self.table[index]:
             for i, item in enumerate(self.table[index]):
@@ -56,6 +81,8 @@ class HashTable:
             self._resize(0.6)
 
     def get(self, key):
+        '''Retrieve an item with the speficied key.
+        '''
         index = self._get_hash(key)
         if self.table[index]:
             for item in self.table[index]:
